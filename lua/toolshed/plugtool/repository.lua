efde190179578b8da0fe8d872fc9af6de3e17b58
Cedric Mamo <vrighter@gmail.com>
2021-12-20 19:20:17 +0100

    updated repository

--- lua/toolshed/plugtool/repository.lua
+++ lua/toolshed/plugtool/repository.lua
@@ -1,38 +1,46 @@
 return {
-    ['hrsh7th/cmp-vsnip'] = {needs = {"hrsh7th/nvim-cmp", "hrsh7th/vim-vsnip"}},
-    ['hrsh7th/cmp-cmdline'] = {needs = {"hrsh7th/nvim-cmp"}},
-    ['hrsh7th/cmp-path'] = {needs = {"hrsh7th/nvim-cmp"}},
-    ['hrsh7th/cmp-buffer'] = {needs = {"hrsh7th/nvim-cmp"}},
+    ['hrsh7th/cmp-vsnip'] = {needs = {"hrsh7th/nvim-cmp", "hrsh7th/vim-vsnip"}, before={"hrsh7th/nvim-cmp"}, after={"hrsh7th/vim-vsnip"}},
+    ['hrsh7th/cmp-cmdline'] = {needs = {"hrsh7th/nvim-cmp"}, before={"hrsh7th/nvim-cmp"}},
+    ['hrsh7th/cmp-path'] = {needs = {"hrsh7th/nvim-cmp"}, before={"hrsh7th/nvim-cmp"}},
+    ['hrsh7th/cmp-buffer'] = {needs = {"hrsh7th/nvim-cmp"}, before={"hrsh7th/nvim-cmp"}},
     ['hrsh7th/cmp-nvim-lsp'] = {
         needs = {
             "hrsh7th/nvim-cmp", "neovim/nvim-lspconfig", "mfussenegger/nvim-dap"
-        }
+        }, before={"hrsh7th/nvim-cmp"}, after={'neovim/nvim-lspconfig', 'mfussenegger/nvim-dap'}
     },
-    ['rcarriga/nvim-dap-ui'] = {needs = {"mfussenegger/nvim-dap"}},
+    ['rcarriga/nvim-dap-ui'] = {needs = {"mfussenegger/nvim-dap"}, after={"mfussenegger/nvim-dap"}},
     ['thehamsta/nvim-dap-virtual-text'] = {
-        needs = {"mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter"}
+        needs = {"mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter"},
+        after = {"mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter"}
     },
-    ["Pocco81/DAPInstall.nvim"] = {needs = {"mfussenegger/nvim-dap"}},
-    ['kyazdani42/nvim-tree.lua'] = {needs = {"kyazdani42/nvim-web-devicons"}},
-    ['rcarriga/nvim-notify'] = {needs = {"nvim-telescope/telescope.nvim"}},
-    ['mfussenegger/nvim-jdtls'] = {needs = {"mfussenegger/nvim-dap"}},
-    ['williamboman/nvim-lsp-installer'] = {needs = {'neovim/nvim-lspconfig'}},
-    ['jbyuki/one-small-step-for-vimkind'] = {needs = {'mfussenegger/nvim-dap'}},
+    ["Pocco81/DAPInstall.nvim"] = {needs = {"mfussenegger/nvim-dap"}, after = {"mfussenegger/nvim-dap"}},
+    ['kyazdani42/nvim-tree.lua'] = {needs = {"kyazdani42/nvim-web-devicons"}, after={"kyazdani42/nvim-web-devicons"}},
+    ['rcarriga/nvim-notify'] = {needs = {"nvim-telescope/telescope.nvim"}, after = {'nvim-telescope/telescope.nvim'}},
+    ['mfussenegger/nvim-jdtls'] = {needs = {"mfussenegger/nvim-dap"}, after={'mfussenegger/nvim-dap'}},
+    ['williamboman/nvim-lsp-installer'] = {needs = {'neovim/nvim-lspconfig'}, after = {'neovim/nvim-lspconfig'}},
+    ['jbyuki/one-small-step-for-vimkind'] = {needs = {'mfussenegger/nvim-dap'}, after={'mfussenegger/nvim-dap'}},
     ["folke/lua-dev.nvim"] = {
-        needs = {'neovim/nvim-lspconfig', 'mfussenegger/nvim-dap'}
+        needs = {'neovim/nvim-lspconfig', 'mfussenegger/nvim-dap'},
+        after = {'neovim/nvim-lspconfig', 'mfussenegger/nvim-dap'}
     },
-    ['nvim-lualine/lualine.nvim'] = {needs = {'kyazdani42/nvim-tree.lua'}},
-    ['arkav/lualine-lsp-progress'] = {needs = {"nvim-lualine/lualine.nvim"}},
+    ['nvim-lualine/lualine.nvim'] = {needs = {'kyazdani42/nvim-tree.lua'}, after={'kyazdani42/nvim-tree.lua'}},
+    ['arkav/lualine-lsp-progress'] = {needs = {"nvim-lualine/lualine.nvim"},after = {"nvim-lualine/lualine.nvim"}},
     ['nvim-telescope/telescope.nvim'] = {
-        needs = {'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter'}
+        needs = {'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter'},
+        after = {'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter'}
     },
     ['nvim-telescope/telescope-dap.nvim'] = {
         needs = {
             'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap',
             'nvim-treesitter/nvim-treesitter'
+        },
+        after = {
+            'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap',
+            'nvim-treesitter/nvim-treesitter'
         }
     },
     ['nvim-telescope/telescope-packer.nvim'] = {
-        needs = {'wbthomason/packer.nvim', 'nvim-telescope/telescope.nvim'}
+        needs = {'wbthomason/packer.nvim', 'nvim-telescope/telescope.nvim'},
+        after = {'wbthomason/packer.nvim', 'nvim-telescope/telescope.nvim'}
     }
 }
