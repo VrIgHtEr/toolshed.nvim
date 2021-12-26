9a53a32379d5f31b7282f6fd2278a1fc83fcd6ee
Cedric Mamo <vrighter@gmail.com>
2021-12-26 10:55:22 +0100

    Added git log parsing function

--- lua/toolshed/git/init.lua
+++ lua/toolshed/git/init.lua
@@ -38,6 +38,27 @@ function git.clone_async(url, opts)
     end)
 end
 
+local function parse_git_line(line)
+    local index = line:find '|'
+    local ret = {}
+    if index then
+        ret.hash = line:sub(1, index - 1)
+    else
+        error "malformed git log string"
+    end
+
+    local index2 = line:find('|', index + 1)
+    if index2 then
+        ret.time = line:sub(index + 1, index2 - 1)
+    else
+        error "malformed git log string"
+    end
+
+    ret.message = line:sub(index2 + 1)
+
+    return ret
+end
+
 function git.update_async(path, opts)
     if type(path) ~= "string" then
         error("Invalid path type. Expected string but got " .. type(path))
@@ -68,10 +89,13 @@ function git.update_async(path, opts)
         ret, err = a.spawn_lines_a({
             "git",
             "log",
-            '--pretty="%h|%aN|%s"',
+            "--date=relative",
+            '--pretty="%H|%ad|%s"',
             "..FETCH_HEAD",
             cwd = path
-        }, function(line) if line then table.insert(output, line) end end)
+        }, function(line)
+            if line then table.insert(output, parse_git_line(line)) end
+        end)
         if not ret then return nil, err end
         if ret ~= 0 then return nil, "Failed to check for new commits" end
 
