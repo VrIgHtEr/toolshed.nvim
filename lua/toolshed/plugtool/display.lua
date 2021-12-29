d238ff385864c2fc193e2d164fadc84bd61095e0
Cedric Mamo <vrighter@gmail.com>
2021-12-29 14:42:45 +0100

    added some colors

--- lua/toolshed/plugtool/display.lua
+++ lua/toolshed/plugtool/display.lua
@@ -26,6 +26,16 @@ function display.new()
         end
     end
 
+    local theme = {
+        title = "Title",
+        commithash = "Label",
+        committime = "Identifier",
+        commitmessage = "String",
+        info = "Normal",
+        error = "Error",
+        warning = "Special"
+    }
+
     local function make_displayer(url)
         local name = url
         local nsname = "toolshed.plugtool.display/" .. name
@@ -38,6 +48,7 @@ function display.new()
         displayers[index] = displayer
         local message = ""
         local changelogs = {}
+        local highlights = {}
 
         function displayer.padurl()
             local len = url:len()
@@ -49,28 +60,52 @@ function display.new()
 
         local function create_lines()
             local newlines = {}
-            for x in message:lines() do table.insert(newlines, x) end
-            for _, x in ipairs(changelogs) do
-                table.insert(newlines, '- ' .. x.hash:sub(1, 8) .. ' - ' ..
-                                 x.time .. ' - ' .. x.message)
+            local newhighlights = {}
+            local indent = emptypadding:len()
+            for x in message:lines() do
+                table.insert(newlines, x)
+                local hl = {{indent, indent + x:len(), theme.info}}
+                table.insert(newhighlights, hl)
             end
-            if #newlines == 0 then table.insert(newlines, "") end
+            for _, x in ipairs(changelogs) do
+                local line = x.hash:sub(1, 8) .. ' - ' .. x.time .. ' - ' ..
+                                 x.message
+                table.insert(newlines, line)
+                local hl = {
+                    {indent + 0, indent + 8, theme.commithash},
+                    {
+                        indent + 8 + 3,
+                        indent + line:len() - x.message:len() - 3,
+                        theme.committime
+                    },
+                    {
+                        indent + line:len() - x.message:len(), -1,
+                        theme.commitmessage
+                    }
+                }
+                table.insert(newhighlights, hl)
+            end
+            if #newlines == 0 then
+                table.insert(newlines, "")
+                table.insert(newhighlights, {})
+            end
             if #newlines > 1 then table.insert(newlines, "") end
+            table.insert(newhighlights[1], {0, name:len(), theme.title})
             newlines[1] = url .. separator .. newlines[1]
             for i = 2, #newlines do
                 newlines[i] = emptypadding .. newlines[i]
             end
-            return newlines
+            return newlines, newhighlights
         end
 
         function displayer.message(str, changes)
             vim.schedule(function()
                 changelogs = changes or {}
                 message = str
-                local newlines = create_lines()
+                local newlines, newhl = create_lines()
                 local last_line = get_last_line()
                 local redraw_following = redraw_all or (#lines ~= #newlines)
-                lines = newlines
+                lines, highlights = newlines, newhl
 
                 local prev
                 if redraw_all then
@@ -96,18 +131,24 @@ function display.new()
         end
 
         function displayer.get_next_line() return lineindex + #lines end
+
         function displayer.set_line_index(idx) lineindex = idx end
+
         function displayer.redraw()
             if not ns then ns = vim.api.nvim_create_namespace(nsname) end
             vim.api.nvim_buf_clear_namespace(buf, ns, lineindex,
                                              displayer.get_next_line())
-            lines = create_lines()
+            lines, highlights = create_lines()
             vim.api.nvim_buf_set_lines(buf, lineindex,
                                        displayer.get_next_line(), false, lines)
-
-            vim.api.nvim_buf_add_highlight(buf, ns, 'Title', lineindex, 0,
-                                           name:len())
+            for i, hl in ipairs(highlights) do
+                for _, x in ipairs(hl) do
+                    vim.api.nvim_buf_add_highlight(buf, ns, x[3],
+                                                   lineindex + i - 1, x[1], x[2])
+                end
+            end
         end
+
         if url:len() > urlwidth then
             urlwidth = url:len()
             for _, x in ipairs(displayers) do x.padurl() end
