4330ed86d1fb14ded543b8fb4ad99479051a1184
Cedric Mamo <vrighter@gmail.com>
2021-12-31 05:54:50 +0100

    updated neovim-cmake config

--- lua/toolshed/plugtool/repository/Pocco81/DAPInstall_nvim.lua
+++ lua/toolshed/plugtool/repository/Pocco81/DAPInstall_nvim.lua
@@ -6,5 +6,12 @@ return {
         dap_install.setup({
             installation_path = require'toolshed.env'.opt .. "/dap/"
         })
+        local dbg_list =
+            require("dap-install.api.debuggers").get_installed_debuggers()
+
+        for _, debugger in ipairs(dbg_list) do
+            dap_install.config(debugger)
+        end
+
     end
 }
--- lua/toolshed/plugtool/repository/Shatur/neovim-cmake.lua
+++ lua/toolshed/plugtool/repository/Shatur/neovim-cmake.lua
@@ -4,10 +4,23 @@ return {
     config = function()
         require'telescope'.load_extension("cmake")
         require('cmake').setup({
-            type = 'codelldb',
-            request = 'launch',
-            stopOnEntry = false,
-            runInTerminal = false
+            dap_configuration = {
+                type = 'codelldb',
+                request = 'launch',
+                stopOnEntry = false,
+                runInTerminal = false
+            }
         })
+        vim.api.nvim_exec([[
+                            augroup NeovimCmakeFormatAutogroup
+                              autocmd!
+                              autocmd BufWritePre *.cpp lua vim.lsp.buf.formatting_sync()
+                              autocmd BufWritePre *.hpp lua vim.lsp.buf.formatting_sync()
+                              autocmd BufWritePre *.c lua vim.lsp.buf.formatting_sync()
+                              autocmd BufWritePre *.h lua vim.lsp.buf.formatting_sync()
+                            augroup end
+                        nnoremap <buffer> <leader>cc :JdtCompile<cr>
+                          ]], true)
+
     end
 }
