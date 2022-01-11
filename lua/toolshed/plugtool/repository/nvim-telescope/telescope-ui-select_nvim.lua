84b1bb133125a08a86a674aaba1e635347e55e94
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-11 12:10:52 +0100

    updated telescope config

--- lua/toolshed/plugtool/repository/nvim-telescope/telescope-ui-select_nvim.lua
+++ lua/toolshed/plugtool/repository/nvim-telescope/telescope-ui-select_nvim.lua
@@ -3,13 +3,9 @@ return {
     before = {'nvim-telescope/telescope.nvim'},
     config = {
         {
-            function(_, state)
-
-                local st = state["nvim-telescope/telescope.nvim"]
-                if st == nil then
-                    st = {}
-                    state["nvim-telescope/telescope.nvim"] = st
-                end
+            function()
+                local st =
+                    require'toolshed.plugtool'.state "nvim-telescope/telescope.nvim"
                 if st.extensions == nil then st.extensions = {} end
                 st.extensions["ui-select"] = {
                     require("telescope.themes").get_dropdown {
--- lua/toolshed/plugtool/repository/nvim-telescope/telescope_nvim.lua
+++ lua/toolshed/plugtool/repository/nvim-telescope/telescope_nvim.lua
@@ -1,10 +1,9 @@
 return {
     needs = {'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter'},
     after = {'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter'},
-    config = function(_, state)
-        local st = state["nvim-telescope/telescope.nvim"]
-        local cfg = {}
-        if st ~= nil then cfg.extensions = st.extensions end
-        require'telescope'.setup(cfg)
+    config = function()
+        require'telescope'.setup {
+            extensions = require'toolshed.plugtool'.state "nvim-telescope/telescope.nvim"
+        }
     end
 }
