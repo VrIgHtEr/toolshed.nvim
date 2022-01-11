cb8023d68d8cce9f14ebbd6bfff82208f1aaa6b1
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-11 12:04:52 +0100

    Updated nvim-jdtls config

--- lua/toolshed/plugtool/repository/mfussenegger/nvim-jdtls.lua
+++ lua/toolshed/plugtool/repository/mfussenegger/nvim-jdtls.lua
@@ -98,42 +98,6 @@ local function setup(state)
     config['on_attach'] = function(_, _)
         require'jdtls'.setup_dap({hotcodereplace = 'auto'})
         require'jdtls.setup'.add_commands()
-
-        local finders = require 'telescope.finders'
-        local sorters = require 'telescope.sorters'
-        local actions = require 'telescope.actions'
-        local pickers = require 'telescope.pickers'
-        local action_state = require 'telescope.actions.state'
-        require('jdtls.ui').pick_one_async =
-            function(items, prompt, label_fn, cb)
-                local opts = {}
-                pickers.new(opts, {
-                    prompt_title = prompt,
-                    finder = finders.new_table {
-                        results = items,
-                        entry_maker = function(entry)
-                            return {
-                                value = entry,
-                                display = label_fn(entry),
-                                ordinal = label_fn(entry)
-                            }
-                        end
-                    },
-                    sorter = sorters.get_generic_fuzzy_sorter(),
-                    attach_mappings = function(prompt_bufnr)
-                        actions.select_default:replace(function()
-                            local selection =
-                                action_state.get_selected_entry(prompt_bufnr)
-
-                            actions.close(prompt_bufnr)
-
-                            cb(selection.value)
-                        end)
-
-                        return true
-                    end
-                }):find()
-            end
     end
     state.setup = function() require'jdtls'.start_or_attach(config) end
     vim.api.nvim_exec([[
