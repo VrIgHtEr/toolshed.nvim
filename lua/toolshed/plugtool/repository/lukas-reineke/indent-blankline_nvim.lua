dcf2383c77ab1956b9b68f2a6075a216babbebee
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-13 09:14:38 +0100

    added configuration for indent_blankline

---
+++ lua/toolshed/plugtool/repository/lukas-reineke/indent-blankline_nvim.lua
@@ -0,0 +1,8 @@
+return {
+    config = function()
+        require('indent_blankline').setup {
+            show_current_context = true,
+            show_current_context_start = true,
+        }
+    end,
+}
