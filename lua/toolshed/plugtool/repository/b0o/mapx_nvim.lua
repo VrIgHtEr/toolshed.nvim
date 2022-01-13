e00eea17681dcbe338aa52f4e05dbef901f8acea
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-13 13:16:17 +0100

    updated some configs

---
+++ lua/toolshed/plugtool/repository/b0o/mapx_nvim.lua
@@ -0,0 +1,10 @@
+return {
+    after = { 'folke/which-key.nvim' },
+    config = function(plugins)
+        local config = { global = true }
+        if plugins['folke/which-key.nvim'] then
+            config.whichkey = true
+        end
+        require('mapx').setup(config)
+    end,
+}
