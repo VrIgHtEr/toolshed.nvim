cbedf0d9e1d1760a4b02b6c2e8a45eec545b4ad2
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-13 17:14:35 +0100

    updated config repo

--- lua/toolshed/plugtool/repository/SmiteshP/nvim-gps.lua
+++ lua/toolshed/plugtool/repository/SmiteshP/nvim-gps.lua
@@ -1,4 +1,15 @@
 return {
     needs = { 'nvim-treesitter/nvim-treesitter' },
-    after = { 'nvim-treesitter/nvim-treesitter' },
+    after = { 'nvim-treesitter/nvim-treesitter', 'nvim-lualine/lualine.nvim' },
+    config = function()
+        local config = require('toolshed.plugtool').state 'nvim-lualine/lualine.nvim'
+        if config.sections == nil then
+            config.sections = {}
+        end
+        if config.sections.lualine_x == nil then
+            config.sections.lualine_x = {}
+        end
+        local gps = require 'nvim-gps'
+        table.insert(config.sections.lualine_x, { gps.get_location, cond = gps.is_available })
+    end,
 }
