2cb44bb0b6fbe22633c186b6c1ca64d35bee77d6
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-19 12:02:48 +0100

    added config for lsp-colors

---
+++ lua/plugtool/repository/folke/lsp-colors_nvim.lua
@@ -0,0 +1,10 @@
+return {
+    config = function()
+        require('lsp-colors').setup {
+            Error = '#db4b4b',
+            Warning = '#e0af68',
+            Information = '#0db9d7',
+            Hint = '#10B981',
+        }
+    end,
+}
