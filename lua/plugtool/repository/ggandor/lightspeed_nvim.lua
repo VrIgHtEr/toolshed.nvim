e2eb3d4a8bc99531a3c9212fbdb3c97c36d3b3ef
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-19 16:35:54 +0100

    reverted change to lightspeed.nvim config, because the default mapleader is now assumed to be "space"

--- lua/plugtool/repository/ggandor/lightspeed_nvim.lua
+++ lua/plugtool/repository/ggandor/lightspeed_nvim.lua
@@ -2,7 +2,4 @@ return {
     requires = { 'tpope/vim-repeat' },
     after = { 'tpope/vim-repeat' },
     before = { 'folke/which-key.nvim' },
-    config = function()
-        vim.api.nvim_exec('silent! unmap ,', true)
-    end,
 }
