429896872c9770a4d324a970e7bc81801c87524e
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-19 12:03:30 +0100

    fixed dependency

--- lua/plugtool/repository/folke/trouble_nvim.lua
+++ lua/plugtool/repository/folke/trouble_nvim.lua
@@ -1,6 +1,6 @@
 return {
     needs = { 'kyazdani42/nvim-web-devicons' },
-    after = { 'kyazdani42/nvim-web-devicons' },
+    after = { 'kyazdani42/nvim-web-devicons', 'folke/lsp-colors.nvim' },
     config = function()
         require('trouble').setup {}
     end,
