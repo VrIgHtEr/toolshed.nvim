b69f34f440121c9a2aaa36626535c4e7cae2c55e
Cedric Mamo <vrighter@gmail.com>
2022-01-19 10:33:10 +0100

    fixed config of rainbowparen plugin

--- lua/plugtool/repository/p00f/nvim-ts-rainbow.lua
+++ lua/plugtool/repository/p00f/nvim-ts-rainbow.lua
@@ -4,25 +4,17 @@ return {
     config = {
         function()
             vim.api.nvim_exec('set termguicolors', true)
+            require('nvim-treesitter.configs').setup {
+                highlight = {},
+                rainbow = {
+                    enable = true,
+                    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+                    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+                    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+                    -- colors = {}, -- table of hex strings
+                    -- termcolors = {} -- table of colour name strings
+                },
+            }
         end,
-        {
-            function()
-                if true then
-                    return
-                end
-                require('nvim-treesitter.configs').setup {
-                    highlight = {},
-                    rainbow = {
-                        enable = true,
-                        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-                        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-                        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-                        -- colors = {}, -- table of hex strings
-                        -- termcolors = {} -- table of colour name strings
-                    },
-                }
-            end,
-            after = 'nvim-treesitter/nvim-treesitter',
-        },
     },
 }
