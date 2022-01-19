8871f1ebeb91d79ff02f388f7a0f1ab2fa89b8ea
Cedric Mamo <vrighter@gmail.com>
2022-01-19 22:55:02 +0100

    added mapping for lsp hover

--- lua/plugtool/repository/neovim/nvim-lspconfig.lua
+++ lua/plugtool/repository/neovim/nvim-lspconfig.lua
@@ -29,6 +29,7 @@ return {
                 nnoremap('<leader>rr', ':lua vim.lsp.buf.rename()<cr>', 'silent', 'Lsp: Rename element')
                 nnoremap('<leader>ca', ':lua vim.lsp.buf.code_action()<cr>', 'silent', 'Lsp: Code action')
                 nnoremap('<leader>gd', ':lua vim.lsp.buf.definition()<cr>', 'silent', 'Lsp: Go to definition')
+                nnoremap('<leader>ch', ':lua vim.lsp.buf.hover()<cr>', 'silent', 'Lsp: Hover')
             end,
             after = 'b0o/mapx.nvim',
         },
