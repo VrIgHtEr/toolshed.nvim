77f9948b4cf0b7ff9dbd54a3b8315641413be930
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-19 13:50:40 +0100

    added a config

--- lua/plugtool/repository/wfxr/minimap_vim.lua
+++ lua/plugtool/repository/wfxr/minimap_vim.lua
@@ -1,8 +1,8 @@
 return {
     preload = function()
         vim.g.minimap_width = 10
-        vim.g.minimap_auto_start = 1
-        vim.g.minimap_auto_start_win_enter = 1
+        vim.g.minimap_auto_start = 0
+        vim.g.minimap_auto_start_win_enter = 0
     end,
     config = function()
         local env = require 'toolshed.env'
