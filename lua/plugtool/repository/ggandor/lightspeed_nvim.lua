32adff77bd60d3ce1f372c3aa11107aec707f863
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-19 16:15:35 +0100

    fixed conflicting keymap

--- lua/plugtool/repository/ggandor/lightspeed_nvim.lua
+++ lua/plugtool/repository/ggandor/lightspeed_nvim.lua
@@ -1,4 +1,8 @@
 return {
     requires = { 'tpope/vim-repeat' },
     after = { 'tpope/vim-repeat' },
+    before = { 'folke/which-key.nvim' },
+    config = function()
+        vim.api.nvim_exec('silent! unmap ,', true)
+    end,
 }
