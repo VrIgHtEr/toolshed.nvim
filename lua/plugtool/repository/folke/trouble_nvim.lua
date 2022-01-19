d0b4fd58735728b39a89d81225e9483990fb8b88
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-19 12:11:14 +0100

    updated configuration for trouble.nvim

--- lua/plugtool/repository/folke/trouble_nvim.lua
+++ lua/plugtool/repository/folke/trouble_nvim.lua
@@ -1,7 +1,20 @@
 return {
-    needs = { 'kyazdani42/nvim-web-devicons' },
-    after = { 'kyazdani42/nvim-web-devicons', 'folke/lsp-colors.nvim' },
+    needs = { 'kyazdani42/nvim-web-devicons', 'nvim-telescope/telescope.nvim' },
+    after = { 'kyazdani42/nvim-web-devicons', 'folke/lsp-colors.nvim', 'nvim-telescope/telescope.nvim' },
     config = function()
         require('trouble').setup {}
+        local actions = require 'telescope.actions'
+        local trouble = require 'trouble.providers.telescope'
+
+        local telescope = require 'telescope'
+
+        telescope.setup {
+            defaults = {
+                mappings = {
+                    i = { ['<c-t>'] = trouble.open_with_trouble },
+                    n = { ['<c-t>'] = trouble.open_with_trouble },
+                },
+            },
+        }
     end,
 }
