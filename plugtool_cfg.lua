ba5f8b96b7901911f1109f424332a0d08ba6e1a6
Cedric Mamo <vrighter@gmail.com>
2022-01-22 09:30:55 +0100

    added sequencing for toolshed.nvim

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -1,20 +1,20 @@
-local constants = require 'plugtool.constants'
+local constants = require("plugtool.constants")
 return {
-    needs = { constants.mapx },
-    after = { constants.cache_plugin_name, constants.mapx },
-    config = {
-        function()
-            nnoremap('<leader>pu', ':lua require"plugtool-setup"()<cr>', 'silent', 'Update all plugins using plugtool')
-        end,
-        {
-            function()
-                local state = require('plugtool').state 'lukas-reineke/indent-blankline.nvim'
-                if not state.excludedfiletypes then
-                    state.excludedfiletypes = {}
-                end
-                table.insert(state.excludedfiletypes, 'plugtool')
-            end,
-            before = 'lukas-reineke/indent-blankline.nvim',
-        },
-    },
+	needs = { constants.mapx },
+	after = { constants.cache_plugin_name, constants.mapx, "vrighter/update.nvim" },
+	config = {
+		function()
+			nnoremap("<leader>pu", ':lua require"plugtool-setup"()<cr>', "silent", "Update all plugins using plugtool")
+		end,
+		{
+			function()
+				local state = require("plugtool").state("lukas-reineke/indent-blankline.nvim")
+				if not state.excludedfiletypes then
+					state.excludedfiletypes = {}
+				end
+				table.insert(state.excludedfiletypes, "plugtool")
+			end,
+			before = "lukas-reineke/indent-blankline.nvim",
+		},
+	},
 }
