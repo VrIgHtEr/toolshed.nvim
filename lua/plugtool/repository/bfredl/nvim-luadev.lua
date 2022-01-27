ff02cb25b2982b0632997c967606bbd2cf86267f
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-27 18:08:08 +0100

    updated nvim-luadev config

--- lua/plugtool/repository/bfredl/nvim-luadev.lua
+++ lua/plugtool/repository/bfredl/nvim-luadev.lua
@@ -2,6 +2,6 @@ return {
     config = function()
         nnoremap('<leader>ldl', '<Plug>(Luadev-RunLine)<cr>', 'silent', 'Luadev: Run line')
         nnoremap('<leader>ldw', '<Plug>(Luadev-RunWord)<cr>', 'silent', 'Luadev: Evaluate identifier under the cursor')
-        nnoremap('<leader>ldr', '<Plug>(Luadev-RunWord)<cr>', 'silent', 'Luadev: Run movement or text object')
+        vnoremap('<leader>ldr', '<Plug>(Luadev-RunWord)<cr>', 'silent', 'Luadev: Run movement or text object')
     end,
 }
