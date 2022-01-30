93bdc544e88dd7ab3c76659b78944dda30fe4edc
Cedric Mamo <vrighter@gmail.com>
2022-01-30 11:54:54 +0100

    fixed bug in lualine config

--- lua/plugtool/repository/bfredl/nvim-luadev.lua
+++ lua/plugtool/repository/bfredl/nvim-luadev.lua
@@ -1,7 +1,7 @@
 return {
     config = function()
-        nnoremap('<leader>ldl', '<Plug>(Luadev-RunLine)<cr>', 'silent', 'Luadev: Run line')
-        nnoremap('<leader>ldw', '<Plug>(Luadev-RunWord)<cr>', 'silent', 'Luadev: Evaluate identifier under the cursor')
-        vnoremap('<leader>ldr', '<Plug>(Luadev-RunWord)<cr>', 'silent', 'Luadev: Run movement or text object')
+        nnoremap('<leader>ldl', '<Plug>(Luadev-RunLine)', 'silent', 'Luadev: Run line')
+        nnoremap('<leader>ldw', '<Plug>(Luadev-RunWord)', 'silent', 'Luadev: Evaluate identifier under the cursor')
+        vnoremap('<leader>ldr', '<Plug>(Luadev-RunWord)', 'silent', 'Luadev: Run movement or text object')
     end,
 }
