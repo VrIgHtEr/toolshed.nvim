faae300b87fb5e0367b1eccf76345d8c0c2ef90a
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-03-02 12:12:14 +0100

    updated nvim-tree config

--- lua/plugtool/repository/kyazdani42/nvim-tree_lua.lua
+++ lua/plugtool/repository/kyazdani42/nvim-tree_lua.lua
@@ -7,29 +7,17 @@ return {
     config = function(plugins)
         vim.api.nvim_exec(
             [[
-let g:nvim_tree_quit_on_open = 1 "0 by default, closes the tree when you open a file
 let g:nvim_tree_indent_markers = 0 "0 by default, this option shows indent markers when folders are open
 let g:nvim_tree_git_hl = 1 "0 by default, will enable file highlight for git attributes (can be used without the icons).
 let g:nvim_tree_highlight_opened_files = 1 "0 by default, will enable folder and file icon highlight for opened files/directories.
 let g:nvim_tree_root_folder_modifier = ':~' "This is the default. See :help filename-modifiers for more options
 let g:nvim_tree_add_trailing = 1 "0 by default, append a trailing slash to folder names
 let g:nvim_tree_group_empty = 1 " 0 by default, compact folders that only contain a single folder into one node in the file tree
-"let g:nvim_tree_disable_window_picker = 1 "0 by default, will disable the window picker.
 let g:nvim_tree_icon_padding = ' ' "one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
 let g:nvim_tree_symlink_arrow = ' -> ' " defaults to ' ➛ '. used as a separator between symlinks' source and target.
 "let g:nvim_tree_respect_buf_cwd = 1 "0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 "let g:nvim_tree_create_in_closed_folder = 0 "1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
 let g:nvim_tree_refresh_wait = 500 "1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
-let g:nvim_tree_window_picker_exclude = {
-    \   'filetype': [
-    \     'notify',
-    \     'packer',
-    \     'qf'
-    \   ],
-    \   'buftype': [
-    \     'terminal'
-    \   ]
-    \ }
 " Dictionary of buffer option names mapped to a list of option values that
 " indicates to the window picker that the buffer's window should not be
 " selectable.
@@ -91,9 +79,9 @@ highlight NvimTreeFolderIcon guibg=blue
             disable_netrw = true,
             hijack_netrw = true,
             open_on_setup = false,
-            ignore_ft_on_setup = {},
             auto_close = true,
             open_on_tab = false,
+            ignore_ft_on_setup = {},
             hijack_cursor = false,
             update_cwd = false,
             update_to_buf_dir = { enable = true, auto_open = true },
@@ -121,6 +109,23 @@ highlight NvimTreeFolderIcon guibg=blue
                 side = 'left',
                 auto_resize = false,
                 mappings = { custom_only = false, list = {} },
+            },
+            actions = {
+                open_file = {
+                    quit_on_open = true,
+                    window_picker = {
+                        exclude = {
+                            filetype = {
+                                'notify',
+                                'packer',
+                                'qf',
+                            },
+                            buftype = {
+                                'terminal',
+                            },
+                        },
+                    },
+                },
             },
         }
     end,
