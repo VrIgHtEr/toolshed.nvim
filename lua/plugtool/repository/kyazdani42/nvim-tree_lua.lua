c9db77d92c02c9d032348e430b47d13b5fabccca
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-04-19 08:46:59 +0200

    updated some configs

--- lua/plugtool/repository/kyazdani42/nvim-tree_lua.lua
+++ lua/plugtool/repository/kyazdani42/nvim-tree_lua.lua
@@ -7,7 +7,6 @@ return {
     config = function(plugins)
         vim.api.nvim_exec(
             [[
-let g:nvim_tree_indent_markers = 0 "0 by default, this option shows indent markers when folders are open
 let g:nvim_tree_git_hl = 1 "0 by default, will enable file highlight for git attributes (can be used without the icons).
 let g:nvim_tree_highlight_opened_files = 1 "0 by default, will enable folder and file icon highlight for opened files/directories.
 let g:nvim_tree_root_folder_modifier = ':~' "This is the default. See :help filename-modifiers for more options
@@ -108,6 +107,9 @@ highlight NvimTreeFolderIcon guibg=blue
                 side = 'left',
                 auto_resize = false,
                 mappings = { custom_only = false, list = {} },
+            },
+            renderer = {
+                enable = true,
             },
             actions = {
                 open_file = {
