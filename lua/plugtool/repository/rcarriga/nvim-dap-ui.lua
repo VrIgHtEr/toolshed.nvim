c7f2ad4902167cb556a41279cb40b39f73b530e1
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-06-20 08:46:02 +0200

    updated nvim-dap-ui config

--- lua/plugtool/repository/rcarriga/nvim-dap-ui.lua
+++ lua/plugtool/repository/rcarriga/nvim-dap-ui.lua
@@ -12,26 +12,25 @@ return {
                 edit = 'e',
                 repl = 'r',
             },
-            sidebar = {
-                -- You can change the order of elements in the sidebar
-                elements = {
-                    -- Provide as ID strings or tables with "id" and "size" keys
-                    {
-                        id = 'scopes',
-                        size = 1.0 / 3, -- Can be float or integer > 1
-                    }, -- {id = "breakpoints", size = 0.25},
-                    { id = 'stacks', size = 1.0 / 3 },
-                    { id = 'watches', size = 1.0 / 3 },
+            layouts = {
+                {
+                    elements = {
+                        'scopes',
+                        'breakpoints',
+                        'stacks',
+                        'watches',
+                    },
+                    size = 40,
+                    position = 'left',
                 },
-                size = 80,
-                position = 'left', -- Can be "left", "right", "top", "bottom"
-            },
-            tray = {
-                elements = {
-                    -- "repl"
+                {
+                    elements = {
+                        'repl',
+                        'console',
+                    },
+                    size = 10,
+                    position = 'bottom',
                 },
-                size = 10,
-                position = 'bottom', -- Can be "left", "right", "top", "bottom"
             },
             floating = {
                 max_height = nil, -- These can be integers or a float between 0 and 1.
