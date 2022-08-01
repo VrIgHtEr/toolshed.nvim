f90aeeab915d858f3d1ae4fe7e9373661a531b50
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-08-01 14:27:40 +0200

    updated some configs

--- lua/plugtool/repository/mfussenegger/nvim-dap.lua
+++ lua/plugtool/repository/mfussenegger/nvim-dap.lua
@@ -1,3 +1,7 @@
+local function lldb_available()
+    return '/usr/bin/lldb-vscode'
+end
+
 return {
     config = function()
         nnoremap('<F5>', ':lua require"dap".continue()<cr>', 'silent', 'Dap: continue')
@@ -8,5 +12,32 @@ return {
         nnoremap('<leader>dB', ':lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<cr>', 'silent', 'Dap: Set conditional breakpoint')
         nnoremap('<leader>dr', ':lua require"dap".repl_open()<cr>', 'silent', 'Dap: Open REPL')
         nnoremap('<leader>dl', ':lua require"dap".run_last()<cr>', 'silent', 'Dap: run last')
+
+        local lldbpath = lldb_available()
+        if lldbpath then
+            local dap = require 'dap'
+            dap.adapters.lldb = {
+                type = 'executable',
+                command = '/usr/bin/lldb-vscode',
+                name = 'lldb',
+            }
+
+            dap.configurations.cpp = {
+                {
+                    name = 'Launch',
+                    type = 'lldb',
+                    request = 'launch',
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                    args = {},
+                },
+            }
+            dap.configurations.c = dap.configurations.cpp
+            dap.configurations.rust = dap.configurations.cpp
+            dap.configurations.zig = dap.configurations.cpp
+        end
     end,
 }
