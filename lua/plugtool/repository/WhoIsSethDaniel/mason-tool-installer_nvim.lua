385ad4489540e6f4c9a329b4f80c0a1dd51be1ed
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-08-29 09:21:18 +0200

    bugfix

--- lua/plugtool/repository/WhoIsSethDaniel/mason-tool-installer_nvim.lua
+++ lua/plugtool/repository/WhoIsSethDaniel/mason-tool-installer_nvim.lua
@@ -2,6 +2,6 @@ return {
     needs = { 'williamboman/mason.nvim' },
     after = { 'williamboman/mason.nvim' },
     config = function()
-        require('mason-tool-installer').setup()
+        require('mason-tool-installer').setup {}
     end,
 }
