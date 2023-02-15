6a62510a964088c520740604de34acaf3d06a8a4
Cedric Mamo <cedric.mamo.1@gov.mt>
2023-02-15 12:53:19 +0100

    updated repo

---
+++ lua/plugtool/repository/VonHeikemen/lsp-zero_nvim.lua
@@ -0,0 +1,35 @@
+return {
+    needs = { 'neovim/nvim-lspconfig',
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lua',
+        'L3MON4D3/LuaSnip',
+        'rafamadriz/friendly-snippets' },
+    before = { 'neovim/nvim-lspconfig',
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lua',
+        'L3MON4D3/LuaSnip',
+        'rafamadriz/friendly-snippets' },
+    config = function()
+        local lsp = require('lsp-zero').preset({
+                name = 'minimal',
+                set_lsp_keymaps = true,
+                manage_nvim_cmp = true,
+                suggest_lsp_servers = true,
+            })
+
+        lsp.nvim_workspace()
+        lsp.setup()
+    end,
+}
--- lua/plugtool/repository/neovim/nvim-lspconfig.lua
+++ lua/plugtool/repository/neovim/nvim-lspconfig.lua
@@ -5,26 +5,5 @@ return {
         nnoremap('<leader>ca', ':lua vim.lsp.buf.code_action()<cr>', 'silent', 'Lsp: Code action')
         nnoremap('<leader>gd', ':lua vim.lsp.buf.definition()<cr>', 'silent', 'Lsp: Go to definition')
         nnoremap('<leader>ch', ':lua vim.lsp.buf.hover()<cr>', 'silent', 'Lsp: Hover')
-        local env = require 'toolshed.env'
-        local dirname = 'lua-language-server'
-        local path = env.get_dependency_path(dirname)
-        return env.install_dependencies {
-            {
-                skip = false,
-                dirname = dirname,
-                repo = 'https://github.com/sumneko/lua-language-server',
-                recurse_submodules = true,
-                buildcmd = {
-                    { 'compile/install.sh', cwd = path .. '/3rd/luamake' },
-                    { '3rd/luamake/luamake', 'rebuild', cwd = path },
-                },
-                launchscript = {
-                    name = 'lua-language-server',
-                    script = [[#!/bin/env sh
-]] .. env.bashescape(path .. '/bin/lua-language-server') .. ' -E ' .. env.bashescape(path .. '/bin/main.lua') .. ' $@',
-                },
-                builddeps = { env.exec_checker 'ninja' },
-            },
-        }
     end,
 }
