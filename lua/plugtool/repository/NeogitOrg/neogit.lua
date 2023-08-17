4b8d6199314e7f5ce7a2edad0f1f088199c64114
Cedric Mamo <vrighter@gmail.com>
2023-08-17 17:07:56 +0200

    updated neogit config

--- lua/plugtool/repository/NeogitOrg/neogit.lua
+++ lua/plugtool/repository/NeogitOrg/neogit.lua
@@ -40,7 +40,7 @@ return {
                 diffview = true,
             },
             -- Setting any section to `false` will make the section not render at all
-            sections = {
+            section = {
                 untracked = {
                     folded = false,
                 },
