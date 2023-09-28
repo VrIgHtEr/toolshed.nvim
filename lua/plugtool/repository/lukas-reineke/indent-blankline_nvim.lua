6ea44d9efc0d7944387da571f2f84125acb604a1
Cedric Mamo <cedric.mamo.1@gov.mt>
2023-09-28 07:40:26 +0200

    updated indent-blankline configuration

--- lua/plugtool/repository/lukas-reineke/indent-blankline_nvim.lua
+++ lua/plugtool/repository/lukas-reineke/indent-blankline_nvim.lua
@@ -10,23 +10,13 @@ return {
         if state.excludedfiletypes then
             local autocmds = { 'augroup plugtool_indentblanklineexcludediletypes', 'autocmd!' }
             for _, x in ipairs(state.excludedfiletypes) do
-                table.insert(autocmds, 'autocmd BufEnter ' .. x .. ' :IndentBlanklineDisable')
-                table.insert(autocmds, 'autocmd BufLeave ' .. x .. ' :IndentBlanklineEnable')
-                table.insert(autocmds, 'autocmd FileType ' .. x .. ' :IndentBlanklineDisable')
+                table.insert(autocmds, 'autocmd BufEnter ' .. x .. ' :IBLDisable')
+                table.insert(autocmds, 'autocmd BufLeave ' .. x .. ' :IBLEnable')
+                table.insert(autocmds, 'autocmd FileType ' .. x .. ' :IBLDisable')
             end
             table.insert(autocmds, 'augroup end')
             vim.api.nvim_exec(table.concat(autocmds, '\n'), true)
         end
-        require('indent_blankline').setup {
-            use_treesitter = true,
-            show_trailing_blankline_indent = true,
-            enabled = true,
-            show_end_of_line = true,
-            show_foldtext = true,
-            show_current_context_start = true,
-            show_current_context_start_on_current_line = true,
-            show_current_context = true,
-            space_char_blankline = ' ',
-        }
+        require('ibl').setup()
     end,
 }
